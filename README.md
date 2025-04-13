16c1a40eff751aed50f76088c710ea853051903f
SEB <sebasgr00@gmail.com>
2025-04-13 17:46:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Challenge del curso "Java: creando tu primera aplicación" de Alura - Oracle One
+
+Este desafío hace parte de la conclusión del curso, donde el estudiante debe crear un programa bancario que permita visualisar saldo, retirar dinero, depositar dinero y salir de la aplicación.
+
+El programa usa estructuras condicionales (`if`, `switch`) y cíclicas (`while`), además de recolectar información del usuario (`Scanner`), tiene varias funciones que ayudan a una mejor visualización y reutilización del código (`deposit`, `showBalance`, `withdraw`, `verifyBalanceToWithdraw`) así como una lógica que asegura el correcto funcionamiento del mismo.
+
+---
+
+Challenge from the course "Java: creating your first application" by Alura - Oracle One
+
+This challenge is part of the conclusion of the course, where the student must create a banking program that allows viewing the balance, withdrawing money, depositing money, and exiting the application.
+
+The program uses conditional structures (`if`, `switch`) and loops (`while`), as well as collecting user input (`Scanner`). It includes several functions that help with better visualization and code reusability (`deposit`, `showBalance`, `withdraw`, `verifyBalanceToWithdraw`), along with logic that ensures the correct operation of the program`.
---
+++ src/Main.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = "CR7";
+        String accountType = "Checking account";
+        double balance = 2000;
+
+        System.out.println("-------------------------------");
+        System.out.println("Welcome to SEB's bank!");
+        System.out.println("Client's name: " + name);
+        System.out.println("Account type: " + accountType);
+        System.out.println("Available balance: $" + balance);
+        System.out.println("------------------------------- \n");
+
+        while (true) {
+            System.out.println("Select an option, enter the respective number.");
+            System.out.println("1 - Watch balance");
+            System.out.println("2 - Withdraw");
+            System.out.println("3 - Deposit");
+            System.out.println("9 - Exit");
+
+            System.out.println("");
+            System.out.print("> ");
+            String option = scanner.nextLine();
+
+            if (option.equals("9")) {
+                System.out.println("Good Bye! We hope to see you again " + name + ".\n");
+                break;
+            }
+
+            switch (option) {
+                case "1":
+                    showBalance(balance);
+                    break;
+                case "2":
+                    System.out.println("What amount would you like to withdraw?");
+                    System.out.print("> $");
+                    double amount = Double.parseDouble(scanner.nextLine());
+                    balance = withdraw(amount, balance);
+                    showBalance(balance);
+                    break;
+                case "3":
+                    System.out.println("What amount are you going to deposit?");
+                    System.out.print("> $");
+                    double amountDeposit = Double.parseDouble(scanner.nextLine());
+                    balance = deposit(amountDeposit, balance);
+                    showBalance(balance);
+                    break;
+                default:
+                    System.out.println("Invalid option, please try again. \n");
+            }
+        }
+
+        scanner.close(); 
+    }
+
+    public static double deposit(double amount, double balance) {
+        if (amount > 0) {
+            return balance + amount;
+        }
+        System.out.println("Incorrect amount!");
+        return balance;
+    }
+
+    public static void showBalance(double balance) {
+        System.out.println("The updated balance is $" + balance + "\n");
+    }
+
+    public static double withdraw(double amount, double balance) {
+        if (amount > 0 && verifyBalanceToWithdraw(balance, amount)) {
+            return balance - amount;
+        } else {
+            System.out.println("Insufficient balance!");
+            return balance;
+        }
+    }
+
+    public static boolean verifyBalanceToWithdraw(double balance, double amount) {
+        return balance >= amount;
+    }
+
+}
